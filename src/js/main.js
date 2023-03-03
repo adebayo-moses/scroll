c3c183adf0506b60a03e24c1d35de5283894c6a1
adebayo-moses <adebayomoses597@gmail.com>
2023-03-03 01:34:54 +0100

    chanting

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
 console.log('Hello Crack Head');
+
+new ScrollyVideo({
+    scrollyVideoContainer: 'scrolly-video',
+    src: 'https://image-us.samsung.com/us/smartphones/galaxy-s23-ultra/videos/galaxy-s23-ultra-highlights-design.webm',
+});
+
+// var img = document.createElement('img');
+// img.src =
+//     'https://image-us.samsung.com/us/smartphones/galaxy-s23-ultra/images/localization/d3eco.png';
+// img.style.width = '100%';
+// img.style.height = '100%';
+// img.style.display = 'block';
+// img.style.margin = '0 auto';
+// img.style.position = 'absolute';
+
+// img.src =
+//     'https://image-us.samsung.com/us/smartphones/galaxy-s23-ultra/images/galaxy-s23-ultra-highlights-spen.jpg';
+// var src = document.getElementById('imagetwo');
+// var src = document.getElementById('imageone');
+// src.appendChild(img);
